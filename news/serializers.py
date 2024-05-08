46bc5deda8a0cfaaae29967439c36ff661208bcb
Yosi <lyt2006@naver.com>
2024-05-08 09:28:36 +0900

    코드 복구

--- news/serializers.py
+++ news/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
 from .models import Comment
 
-class UserSerializer(serializers.ModelSerializer):
+class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = ["content", "created_at", "updated_at"]
+        
+    def create(self, validated_data):
+        news_id = self.context.get('view').kwargs.get('news_id')
+        news = News.objects.get(id=news_id)
+        validated_data['news'] = news
+        return super().create(validated_data)
 
--- news/urls.py
+++ news/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("api/news/<int:pk>/comments/", views.CommentGetPost.as_view()),
-    path("/api/news/<int:pk>/comments/delete/", views.CommentPutDelete.as_view()),
+    path("api/news/<int:new_pk>/comments/", views.CommentGetPost.as_view()),
+    path("/api/news/<int:new_pk>/comments/<int:comment_pk>/", views.CommentPutDelete.as_view()),
 ]
