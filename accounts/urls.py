20f848a31eae2340565ec5d1a1d63771d1e5df41
kimmokdong77 <kimmokdong77@gmail.com>
2024-05-08 20:26:41 +0900

    잘못 쓴 거 수정

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,7 +7,6 @@ from rest_framework_simplejwt.views import (
 from . import views
 
 
-urlpatterns = []
 urlpatterns = [
     path("", views.UserJoinView.as_view()),
     path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
